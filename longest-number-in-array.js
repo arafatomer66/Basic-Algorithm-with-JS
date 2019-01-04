9805248126597bc17e4939caa88515e8257f9f28
arafat <arafatomer66@gmail.com>
2019-01-04 21:39:27 +0600

    longest number in array

---
+++ longest-number-in-array.js
@@ -0,0 +1,33 @@
+function largestOfFour(arr) {
+    var largestNumber = [0,0,0,0];
+    for(var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++) {
+     for(var subArrayIndex = 0; subArrayIndex < arr[arrayIndex].length; subArrayIndex++) {
+        if(arr[arrayIndex][subArrayIndex] > largestNumber[arrayIndex]) {         
+           largestNumber[arrayIndex] = arr[arrayIndex][subArrayIndex];
+         }
+     }
+  }
+  return largestNumber;
+ }
+ largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+
+
+ function largestOfFour(mainArray) {
+    return mainArray.map(function (subArray){
+      return subArray.reduce(function (previousLargestNumber, currentLargestNumber) {
+        return (currentLargestNumber > previousLargestNumber) ? currentLargestNumber : previousLargestNumber;
+      }, 0);
+    });
+  }
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+  
+
+
+  function largestOfFour(mainArray) {
+    return mainArray.map(function(subArray) {
+      return Math.max.apply(null, subArray);
+    });
+  }
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
