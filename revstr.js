2a4c93b07cefb5f3ead58c9b6731096d75a2d88c
Arafat <arafatomer66@gmail.com>
2019-01-13 16:37:06 +0600

    commit

--- revstr.js
+++ revstr.js
@@ -5,3 +5,12 @@ function revstr(value) {
     }
     return newStr;
   }
+
+
+  function revstr(value) {
+    let newStr = "";
+    for (var i = value.length - 1; i >= 0; i--) {
+      newStr += value.charAt(i);
+    }
+    return newStr;
+  }
