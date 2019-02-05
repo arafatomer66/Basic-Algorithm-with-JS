6346568f9f26c2c45a54c90871572a5fff76cb25
Arafat <arafatomer66@gmail.com>
2019-02-05 11:58:51 +0600

    chunky monky

---
+++ chunky-Monky.js
@@ -0,0 +1,14 @@
+function chunkArrayInGroups(arr, size) {
+
+    var group = [];
+    var position = 0;
+
+    while (position < arr.length) 
+            {
+                group.push(arr.slice(position, position += size));
+                console.log(position);
+                console.log(group);
+            }
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 3);
