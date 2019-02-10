b10b74a53a58014a563c8218397acf3ffbf99069
Arafat <arafatomer66@gmail.com>
2019-02-10 13:02:48 +0600

    magic with array

---
+++ wheredoibelong.js
@@ -0,0 +1,11 @@
+function getIndexToIns(arr, num) {
+    arr.sort(function(a,b){
+      return a - b;
+    });
+    for (i = 0; i < arr.length; i++){
+      if(num <= arr[i]){
+        return i;  
+        } 
+    } return arr.length; 
+  }
+  getIndexToIns([10, 20, 30, 40, 50], 35); 
