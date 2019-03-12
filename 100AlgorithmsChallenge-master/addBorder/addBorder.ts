77448324c5c78e846afdb0b08951fd5085bb51b7
Arafat <arafatomer66@gmail.com>
2019-03-12 16:02:45 +0600

    wall

--- 100AlgorithmsChallenge-master/addBorder/addBorder.ts
+++ 100AlgorithmsChallenge-master/addBorder/addBorder.ts
@@ -1,5 +1,19 @@
-function addBorder(picture: string[]): string[] {
+function addBorder(picture: string[]): string[] 
+{   let wall = '*'.repeat(picture[0].length + 2);
+//    let lengthOfWall =  picture[0].length +2 ;
 
+//    for(let i = 0 ; i < lengthOfWall ; i++){
+//      wall =  wall.concat('*');
+//    }
+   picture.unshift(wall);
+   picture.push(wall);
+
+   for(let i = 1 ; i < picture.length - 1; i++ ){
+      picture[i] = '*'.concat(picture[i] , '*');
+   }
+
+   return picture ;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+
+ console.log(addBorder(["abc", "ded"]));
