716d9962bde08489f0464643d8b8d838a5784b67
Arafat <arafatomer66@gmail.com>
2019-03-14 17:47:48 +0600

    str

--- 100AlgorithmsChallenge-master/alphabeticShift/alphabeticShift.ts
+++ 100AlgorithmsChallenge-master/alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,11 @@
+
 function alphabeticShift(inputString: string): string {
-
+  let arr = (inputString.split(''));
+  let newArr = []; 
+  arr.forEach(element => {
+     newArr.push(String.fromCharCode(element.charCodeAt(0) + 1)); 
+  });
+  ;
+  return newArr.join('') ;
 }
-
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('abcd'));
