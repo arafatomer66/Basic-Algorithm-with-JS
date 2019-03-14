043d3aa0cd14a2e8e18d2816bd27c88c5b4da1a5
Arafat <arafatomer66@gmail.com>
2019-03-14 11:16:24 +0600

    rest

--- 100AlgorithmsChallenge-master/allLongestStrings/allLongestStrings.ts
+++ 100AlgorithmsChallenge-master/allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,21 @@
 function allLongestStrings(inputArray: string[]): string[] {
 
+    let longestWord = 0 ;
+
+    const arr = [ ] ;
+    
+    inputArray.forEach((word : string) => {
+        if(word.length >= longestWord){
+            longestWord = word.length ;
+        }
+    });
+    
+    for(let i = 0 ; i < inputArray.length ; i++){
+        if (inputArray[i].length === longestWord){
+            arr.push(inputArray[i]);
+        }
+    }
+    return arr ;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa" ,'abc','abcd' , "a" ,"asdfg"]));
