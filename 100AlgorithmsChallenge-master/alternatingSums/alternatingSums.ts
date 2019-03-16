1f51ecf6329afb249845ba412001940c4bff637b
Arafat <arafatomer66@gmail.com>
2019-03-16 12:29:10 +0600

    adding odd and even index

--- 100AlgorithmsChallenge-master/alternatingSums/alternatingSums.ts
+++ 100AlgorithmsChallenge-master/alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 function alternatingSums(a: number[]): number[] {
-
+    let evenNum = 0 ;
+    let OddNum = 0 ;
+    
+    a.forEach((element , index) => {
+         if(index %2 === 0){
+             evenNum = evenNum + element
+         }else {
+             OddNum = OddNum + element 
+         }
+    });
+    
+  return [evenNum , OddNum] ;
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([1,2,3]))
