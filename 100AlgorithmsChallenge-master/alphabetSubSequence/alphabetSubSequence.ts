9da88064af999e836bb424fbc2e55e7897d0d37f
Arafat <arafatomer66@gmail.com>
2019-03-16 10:54:22 +0600

    alpha order

--- 100AlgorithmsChallenge-master/alphabetSubSequence/alphabetSubSequence.ts
+++ 100AlgorithmsChallenge-master/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,26 @@
 function alphabetSubsequence(s: string): boolean {
+    const char : string[] = s.split('') ;
+    const num : number[] = [] ;
+    char.forEach(element => {
+        num.push(element.charCodeAt(0));
+    });
+    console.log(num) ;
+     //duplicate value detection
+    if(new Set(num).size != num.length){
+        return false ;
+    }
 
+    for(let i = 0 ; i < num.length ; i++){
+        if(num[i] >= num[i+1] ){
+            return false ;
+        }
+    }
+
+    return true ;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('abc')) ;
+// console.log(alphabetSubsequence('effg'))
+// console.log(alphabetSubsequence('cdce'))
+// console.log(alphabetSubsequence('ace'))
+// console.log(alphabetSubsequence('bxz'))
