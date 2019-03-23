9689fd64e5ae4f691cfa3ccdc20596733cedc849
Arafat <arafatomer66@gmail.com>
2019-03-23 10:30:11 +0600

    array replace

--- 100AlgorithmsChallenge-master/arrayReplace/arrayReplace.ts
+++ 100AlgorithmsChallenge-master/arrayReplace/arrayReplace.ts
@@ -1,5 +1,19 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+    let newArr : number[] = [];
+    inputArray.map(element => {
+       if(element === elemToReplace){
+           newArr.push(substitutionElem);
+       }else {
+           newArr.push(element);
+       }
+   });
+
+//    inputArray.map( (element,index) => {
+//     if(element === elemToReplace){
+//         inputArray[index] = substitutionElem
+//     }
+// });
+   return newArr ;
 }
 
-console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 2, 3));
