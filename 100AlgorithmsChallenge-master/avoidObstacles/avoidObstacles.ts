a0eae7be9bf61fca16dbcb80bf1c3a742bfd452d
Arafat <arafatomer66@gmail.com>
2019-03-24 10:03:40 +0600

    math.floor

--- 100AlgorithmsChallenge-master/avoidObstacles/avoidObstacles.ts
+++ 100AlgorithmsChallenge-master/avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,6 @@
-function avoidObstacles(inputArray: number[]): number {
-
+function avoidObstacles(inputArray: number[]): number { 
+    inputArray =  inputArray.sort((a,b) => a -b) ;
+    return  ;
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
