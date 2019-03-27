b6576f7a1ae237246b37f5b3ed5989f378b8242b
Arafat <arafatomer66@gmail.com>
2019-03-27 14:54:50 +0600

    duplicate number in array

--- 100AlgorithmsChallenge-master/containsDuplicates/containsDuplicates.ts
+++ 100AlgorithmsChallenge-master/containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,11 @@
-function containsDuplicates(a: number[]): boolean {
-
+function containsDuplicates(a: number[]) {
+    a = a.sort((a,b) => a-b) ;
+    for(let i = 0 ; i < a.length ; i++){
+        if(a[i] == a[i+1]){
+            return false ;
+        }
+    }
+    return true  ;
 }
-
 console.log(containsDuplicates([1, 2, 3, 1]));
 console.log(containsDuplicates([3, 1]));
