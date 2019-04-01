6e02b415e0bfc02f859b30cabd1b59d15e8af5e5
Arafat <arafatomer66@gmail.com>
2019-04-01 10:36:19 +0600

    echange

---
+++ Algo-Prac/exchange.js
@@ -0,0 +1,9 @@
+//preety code for exchanging between varibales
+
+
+let a = 10;
+let b = 5;
+let c = 6;
+[b, c, a] = [a, b, c];
+console.log(`a = ${a}`, `b = ${b}`, `c = ${c}`); 
+//a = 6 b = 10 c = 5
