704270fa4b6462c2f7cca0d1398f1431040b257f
Arafat <arafatomer66@gmail.com>
2019-04-06 14:15:13 +0600

    random

---
+++ Algo-Prac/random.js
@@ -0,0 +1,18 @@
+
+//random array 
+var items = [12, 548 , 'a' , 2 , 5478 , 'foo' , 8852, , 'Doe' , 2145 , 119];
+
+var  randomItem = items[Math.floor(Math.random() * items.length)];
+
+//Get a random number in a specific range
+
+var x = Math.floor(Math.random() * (max - min + 1)) + min;
+
+//Generate a random set of alphanumeric characters
+
+function generateRandomAlphaNum(len) {
+    var rdmString = "";
+    for( ; rdmString.length < len; rdmString  += Math.random().toString(36).substr(2));
+    return  rdmString.substr(0, len);
+
+}
