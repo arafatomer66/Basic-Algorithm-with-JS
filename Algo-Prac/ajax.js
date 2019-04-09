dba537379a1229259b72db0c8961898d745c52a6
Arafat <arafatomer66@gmail.com>
2019-04-09 10:17:12 +0600

    -

---
+++ Algo-Prac/ajax.js
@@ -0,0 +1,12 @@
+ // base url
+ const fetch = require("node-fetch");
+ var base = 'https://jsonplaceholder.typicode.com';
+
+ // use fetch on the /posts route, then pass the response along
+ fetch(base + "/posts").then(function(response) {
+     // with the response, convert it to JSON, then pass it along
+     response.json().then(function(json) {
+         // print that JSON
+         console.log(json);
+     });
+ });
--- Algo-Prac/rounding.js
+++ Algo-Prac/rounding.js
@@ -2,3 +2,7 @@ function numRound(floutNumber){
     const absoluteNum = Math.trunc(floutNumber);
     return absoluteNum ;
 }
+
+
+
+console.log(numRound(6.1));
