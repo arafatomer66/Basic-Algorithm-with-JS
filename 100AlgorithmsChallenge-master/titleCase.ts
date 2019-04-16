480086705ab2beada62c86808f2da1336c8938f9
Arafat <arafatomer66@gmail.com>
2019-04-16 15:35:29 +0600

    -

---
+++ 100AlgorithmsChallenge-master/titleCase.ts
@@ -0,0 +1,11 @@
+
+
+const nameStr : string  =  "aRafat" ;
+const lower : string = nameStr.toLowerCase();
+const first : string = lower.charAt(0) ; 
+const upper : string = first.toUpperCase();
+const remain : string = lower.slice(1);
+
+function titleCase(str){
+  return str.charAt(0).toUpperCase() + lower 
+}
