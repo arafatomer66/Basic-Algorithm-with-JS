acdb99e371dbb3c94fb0089ac31ec2b9c1d6c973
Arafat <arafatomer66@gmail.com>
2019-04-17 15:12:33 +0600

    -

--- 100AlgorithmsChallenge-master/extractMatrixColumn/extractMatrixColumn.ts
+++ 100AlgorithmsChallenge-master/extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,11 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+   const  matrixColumn : number[] = [] ;
 
+   matrix.forEach((element) => {
+       matrixColumn.push(element[column])
+   });
+
+   return matrixColumn ;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
